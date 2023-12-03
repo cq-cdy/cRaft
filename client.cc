165a340711bb3f01592861706fa30aa5eac171fe
cdy <1044779679@qq.com>
2023-12-03 13:57:27 +0800

    update README

--- client.cc
+++ client.cc
@@ -6,14 +6,19 @@ int main(int argc, char **argv) {
     spdlog::set_level(spdlog::level::debug);
 
     static int count = 0;
-
+    std::string command =
+            "{"
+            "\"id\":\"auvm_sk_4312768ds1\""
+            ",\"op\":\"delete\""
+            ",\"from\":\"172.16.66.100:12345\""
+            "}";
     //simple stress test
     for (int i = 0; i < 16; i++) {
         std::thread([&] {
             while (true) {
                 CRaftClient client;
-                for(int j =0 ; j< 1000000;j++){
-                    ClientResult res = client.submitCommand("modify a data");
+                for (int j = 0; j < 1000000; j++) {
+                    ClientResult res = client.submitCommand(command);
                     printf("count = %d \n", count++);
                 }
             }
